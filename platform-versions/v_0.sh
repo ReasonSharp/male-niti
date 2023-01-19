7ee87c15ea4f4893a949fff198ffa593d5f571b6
Nikola Novak <reasonsharp@gmail.com>
2023-01-19 18:42:35 +0100

    Prepped for version 1

--- platform-versions/v_0.sh
+++ platform-versions/v_0.sh
@@ -2,6 +2,20 @@
 
 export NGINX_VERSION=1.23.3
 export CERTBOT_VERSION=v1.32.2
+export NODE_VERSION=18.13.0-alpine3.16
 export MBAPP_VERSION=latest
 
-export PLATFORM_COMMIT="fde905c"
+# DBUpdater
+export DBUPDATER_COMMIT="1278320"
+export DBUPDATER_FOLDER="./scripts/dbupdater"
+export DBUPDATER_BRANCH=master
+
+# MB-APP
+export MBAPP_COMMIT=87eacde
+export MBAPP_FOLDER="./sources/mb-app"
+export MBAPP_BRANCH=main
+
+# WWW
+export WWW_COMMIT=6d49868
+export WWW_FOLDER="./sources/www"
+export WWW_BRANCH=main
