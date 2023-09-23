145c73a476e1958ee639d34dd318f58c423d1311
Nikola Novak <novak.nikola@proton.me>
2023-09-23 07:28:36 +0200

    remove mailserver, php, mb-app; fix mb-app build for previous version

--- config/php/smtp.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
- 'websrv' => '${WEB_SERVER_NAME}',
- 'server' => '${SMTP_SERVER}',
- 'user'   => '${SMTP_USER}',
- 'pass'   => '${SMTP_PASS}',
- 'recv'   => '${RECEIVER}'
-);
-?>
